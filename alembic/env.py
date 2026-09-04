aee982b58eae6351de450a0163da70489179a2de
Hung-Bay <jerryga2406@gmail.com>
2026-09-04 15:01:59 +0700

    Khoi tao alembic migration

---
+++ alembic/env.py
@@ -0,0 +1,71 @@
+import model  # Import toàn bộ model (Image, Comment, Category...)
+from db import Base
+import asyncio
+from logging.config import fileConfig
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
+from alembic import context
+
+# 1. Import Base từ db và các Model để Alembic quét qua metadata
+import sys
+from pathlib import Path
+
+# 1. Thêm đường dẫn tới thư mục src/fastapi_vd3 vào sys.path
+BASE_DIR = Path(__file__).resolve().parents[1]
+sys.path.append(str(BASE_DIR / "src" / "fastapi_vd3"))
+
+
+# This is the Alembic Config object
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# Gán metadata để hỗ trợ tính năng Autogenerate
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    """Chạy migration ở chế độ offline (sinh file SQL)."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations():
+    """Chạy migration bất đồng bộ (Async)."""
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    """Chạy migration ở chế độ online (kết nối CSDL trực tiếp)."""
+    asyncio.run(run_async_migrations())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
