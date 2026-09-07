0d89b564e6328f97444be28b5c8d7fa73db20934
Hung-Bay <jerryga2406@gmail.com>
2026-09-07 15:38:29 +0700

    Cau hinh database

--- src/fastapi_vd3/db.py
+++ src/fastapi_vd3/db.py
@@ -8,19 +8,26 @@ load_dotenv()
 
 DATABASE_URL = "postgresql+asyncpg://postgres:8521@localhost:5432/imagedb"
 
-class Base(DeclarativeBase): #Định nghĩa các mô hình bảng sau này
+
+class Base(DeclarativeBase):  # Định nghĩa các mô hình bảng sau này
     pass
 
-#hởi tạo engine bất đồng bộ và bộ tạo session (async_sessionmaker) để giao tiếp với DB
-#expire_on_commit=False giúp dữ liệu không bị xoá khỏi bộ nhớ tạm sau khi commit
-engine = create_async_engine(DATABASE_URL, echo=True) 
+
+# Khởi tạo engine bất đồng bộ và bộ tạo session (async_sessionmaker) để giao tiếp với DB
+# expire_on_commit=False giúp dữ liệu không bị xoá khỏi bộ nhớ tạm sau khi commit
+engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
+
 async def create_db_and_tables():
-    import model as model
+    import Category.models
+    import Image.models
+    import Comment.models
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-async def get_async_session() -> AsyncGenerator[AsyncSession, None]: #Hàm Dependency Generator cấp phát và đóng kết nối session cho từng request một cách an toàn.
+
+# Hàm Dependency Generator cấp phát và đóng kết nối session cho từng request một cách an toàn.
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
