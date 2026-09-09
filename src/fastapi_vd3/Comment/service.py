fe68418e20f7ce3607d74c799051c8e322d8f702
Hung-Bay <jerryga2406@gmail.com>
2026-09-09 09:46:49 +0700

    Binh luan - Comment

--- src/fastapi_vd3/Comment/service.py
+++ src/fastapi_vd3/Comment/service.py
@@ -3,6 +3,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from Comment.models import Comment
 from Comment.schemas import CommentCreate
+from Image.models import Image
+
+
+async def get_image_by_id(image_id: uuid.UUID, session: AsyncSession):
+    stmt = select(Image).where(Image.id == image_id)
+    result = await session.execute(stmt)
+    return result.scalar_one_or_none()
 
 
 async def get_comments_by_id(comment_id: uuid.UUID, session: AsyncSession):
@@ -10,10 +17,12 @@ async def get_comments_by_id(comment_id: uuid.UUID, session: AsyncSession):
     result = await session.execute(stmt)
     return result.scalar_one_or_none()
 
+
 async def get_comments_by_image_id(image_id: uuid.UUID, session: AsyncSession):
     stmt = select(Comment).where(Comment.image_id == image_id)
     result = await session.execute(stmt)
     return result.scalars().all()
+
 
 async def create_comment(comment: CommentCreate, image_id: uuid.UUID, session: AsyncSession):
     new_comment = Comment(
@@ -26,9 +35,11 @@ async def create_comment(comment: CommentCreate, image_id: uuid.UUID, session: A
     await session.refresh(new_comment)
     return new_comment
 
+
 async def delete_comment(comment: Comment, session: AsyncSession):
     await session.delete(comment)
     await session.commit()
+
 
 async def update_comment(comment: Comment, updated_comment: CommentCreate, session: AsyncSession):
     comment.content = updated_comment.content
--- src/fastapi_vd3/Comment/views.py
+++ src/fastapi_vd3/Comment/views.py
@@ -9,33 +9,41 @@ from Comment.schemas import CommentCreate, CommentResponse
 
 router = APIRouter(tags=["Comments"])
 
-@router.get("/images/{image_id}/comments", response_model=CommentResponse)
+
+@router.get("/images/{image_id}/comments", response_model=list[CommentResponse])
 async def get_comment(image_id: uuid.UUID, session: AsyncSession = Depends(get_async_session)):
     image = await service.get_image_by_id(image_id, session)
     if not image:
-        raise HTTPException(status_code=404, detail="Không tìm thấy ảnh với ID đã cho")
-    return await service.get_comments_by_id(image.id, session)
+        raise HTTPException(
+            status_code=404, detail="Không tìm thấy ảnh với ID đã cho")
+    return await service.get_comments_by_image_id(image.id, session)
+
 
 @router.post("/images/{image_id}/comments", response_model=CommentResponse)
 async def create_comment(image_id: uuid.UUID, comment: CommentCreate, session: AsyncSession = Depends(get_async_session)):
     image = await get_image_by_id(image_id, session)
     if not image:
-        raise HTTPException(status_code=404, detail="Không tìm thấy ảnh với ID đã cho")
+        raise HTTPException(
+            status_code=404, detail="Không tìm thấy ảnh với ID đã cho")
     new_comment = await service.create_comment(comment, image_id, session)
     return new_comment
 
-@router.delete("/comments/{comment_id}", response_model=CommentResponse)
+
+@router.delete("/comments/{comment_id}")
 async def delete_comment(comment_id: uuid.UUID, session: AsyncSession = Depends(get_async_session)):
     comment = await service.get_comments_by_id(comment_id, session)
     if not comment:
-        raise HTTPException(status_code=404, detail="Không tìm thấy bình luận với ID đã cho")
+        raise HTTPException(
+            status_code=404, detail="Không tìm thấy bình luận với ID đã cho")
     await service.delete_comment(comment, session)
     return {"message": "Bình luận đã được xóa thành công."}
+
 
 @router.patch("/comments/{comment_id}", response_model=CommentResponse)
 async def update_comment(comment_id: uuid.UUID, updated_comment: CommentCreate, session: AsyncSession = Depends(get_async_session)):
     comment = await service.get_comments_by_id(comment_id, session)
     if not comment:
-        raise HTTPException(status_code=404, detail="Không tìm thấy bình luận với ID đã cho")
+        raise HTTPException(
+            status_code=404, detail="Không tìm thấy bình luận với ID đã cho")
     updated_comment = await service.update_comment(comment, updated_comment, session)
     return updated_comment
